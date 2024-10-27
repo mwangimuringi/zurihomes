870f693284b3c83b5ba0e20bd8b29ddebdd0c73b
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-27 11:46:48 +0300

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="text-center text-white bg-gradient-to-r from-blue-500 to-indigo-500 w-screen h-screen">
+      <h1 className="text-4xl font-bold">Zurihomes</h1>
     </div>
   )
 }
