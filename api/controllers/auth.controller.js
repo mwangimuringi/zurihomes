b722a01a3f170d5adb196724415d92dc97fb4f42
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-03 21:55:13 +0300

    Hashed the passwords using bcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,12 @@
-export const signup = (req, res) => {
-    console.log(req.body);
-    
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    //hashing password
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    //saving info in db user model
+    const newUser = new User({ username, email, password: hashedPassword });
+       await newUser.save()
+       res.status(201).json("User created successfully");
 }
