a086d0019196b65db999ff5e93fde94f65e2bab5
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-03 21:07:30 +0300

    Connected to insomnia api
    Co-authored-by: mwangimuringi <mwangimuringi10@gmail.com>
    Co-authored-by: Julius Mwangi <140041067+mwangimuringi@users.noreply.github.com>
    Co-authored-by: muringitech <muringitech@gmail.com>

--- api/index.js
+++ api/index.js
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
