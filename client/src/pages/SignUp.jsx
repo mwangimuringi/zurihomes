ac0f5a41afdde329eeb94462c783f7a1aff6adec
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-04 23:24:57 +0300

    Styled sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import Link from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' className='border rounded-lg p-3' id="username" />
-        <input type="email" placeholder='Email' className='border rounded-lg p-3' id="email" />
-        <input type="password" placeholder='Username' className='border rounded-lg p-3' id="password" />
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border rounded-lg p-3"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border rounded-lg p-3"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Username"
+          className="border rounded-lg p-3"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
       </form>
+      <div className="flex gap-2 mt-5">
+        <p className="text-center text-sm text-slate-500">
+          Already have an account? <Link href="/sign-in">
+          <span className="text-blue-500">Sign In</span>
+          </Link>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
