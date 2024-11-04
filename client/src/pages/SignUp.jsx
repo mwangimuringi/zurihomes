a528b787b2c381d89d1510d95b54bb592959ebff
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-04 22:37:10 +0300

    Added inputs in signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function SignUp() {
   return (
     <div>
-      Sign-Up
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' className='border rounded-lg p-3' id="username" />
+        <input type="email" placeholder='Email' className='border rounded-lg p-3' id="email" />
+        <input type="password" placeholder='Username' className='border rounded-lg p-3' id="password" />
+      </form>
     </div>
   )
 }
