4d6fd2a1a657807170e74881d0846754cf663baa
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-08 17:23:13 +0300

    Ensured both password and email are correct

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,15 +18,11 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
-    }
-    const isMatch = await bcryptjs.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ success: false, message: "Invalid password" });
-    }
-    res.status(200).json({ success: true, message: "Login successful" });
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    
   } catch (error) {
     next(error);
   }
