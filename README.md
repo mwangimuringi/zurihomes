ceeacb2772905820daa54d9c3234ddccb2775788
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-08 17:37:49 +0300

    Authentication using a cookie

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 mongodb+srv://mwangimuringi:NQ66IWJG5Lmi8u1M@zurihomes.rpw0y.mongodb.net/
 
 install npm i dotenv to use env variables on backend
+
+
+authentication in sign up. Used a cookie in web browser. installed jsonwebtoken
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -22,7 +24,13 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid password"));
-    
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
