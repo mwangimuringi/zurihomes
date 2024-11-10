d75979cdd1c6010d5ed13778008f21dd9b0eb9d9
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-10 22:20:55 +0300

    Integrated user reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./User/userSlice.js";
 
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
 export default configureStore({
   reducer: {
     user: userReducer,
