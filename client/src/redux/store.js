f0e0a5b97761c04669f6a65d077b0f957f679a30
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-10 22:27:20 +0300

    ...

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./User/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
+
+const persitedReducer = persistReducer(persistConfig, rootReducer);
 
 export default configureStore({
   reducer: {
