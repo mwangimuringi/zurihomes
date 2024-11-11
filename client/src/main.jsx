b5f18d441321431368d274394ee598fe34fdb9f5
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-11 20:56:14 +0300

    Imported correctly

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store.js'
+import store from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
