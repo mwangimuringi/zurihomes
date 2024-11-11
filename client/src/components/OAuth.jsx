294290263485fda9616e30152aebbc11fd29c7d8
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-11 22:19:12 +0300

    Sending user auth-data to server

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../firebase";
 
 export default function OAuth() {
   const handleGoogleClick = async () => {
     try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      // popover window for sign in
+      const result = await auth.signInWithPopup(auth, provider);
+
+      //sending data to server
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: result.user.displayName,
+            email: result.user.email,
+            photo: result.user.photoURL,
+        }),
+      } )
     } catch (error) {
       console.log("could not connect to google", error);
     }
