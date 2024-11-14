7027b8e95088d309b3a9b058c3dd66f4e992a83e
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-14 21:19:26 +0300

    Styled profile page ui.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div>
-      Profile
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col '>
+        <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+      </form>
     </div>
   )
 }
