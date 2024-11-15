faf0005a18382501025a4832b8acbfdbb3a884cf
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-15 19:42:08 +0300

    Added image upload component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 
 export default function Profile() {
+  const fileRef = useRef(null)
   const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input src="file" ref={fileRef} hidden accept="image/*"/>
         <img
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
