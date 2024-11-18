ff96cbd64a445106ced311a6ea0dd9e9c033780f
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-18 22:42:49 +0300

    Used refs to avoid uploading duplicate images

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
+import { getStorage } from "../firebase";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -18,7 +19,9 @@ export default function Profile() {
     }
   }, [file]);
   const handleFileUpload = (file) => {
-    const storage = getStorage();
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
