6a2348d65731806a65035dad1b939653856c2884
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-21 23:36:52 +0300

    Improve RESTful Compliance for Profile Update Endpoint.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,25 @@
+// import express from "express";
+// import { test, updateUser } from "../controllers/user.controller.js";
+
+// const router = express.Router();
+
+// router.get('/test', test);
+// router.post('/update/:id', updateUser);
+
+// export default router;
+
 import express from "express";
 import { test, updateUser } from "../controllers/user.controller.js";
+import { validateUpdateProfile } from "../middlewares/validation.js"; 
+import { authenticateUser } from "../middlewares/auth.js"; 
 
 const router = express.Router();
 
+// Test route
 router.get('/test', test);
-router.post('/update/:id', updateUser);
+
+// Update profile route (partial updates)
+router.patch('/update/:id', authenticateUser, validateUpdateProfile, updateUser);
 
 export default router;
+
