bdcb24e2915274e680e1f8da159720e4480b2ca0
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-24 22:00:14 +0300

    Tested the api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -19,14 +19,18 @@ export const updateUser = async (req, res, next) => {
         if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
-   const updateUser = await User.findByIdAndUpdate(req.params.id, {
+   const updatedUser = await User.findByIdAndUpdate(req.params.id, {
     $set:{
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
         avatar: req.body.avatar,
     }
-  });
+  }, {new : true});
+
+  const { password, ...rest} = updatedUser._doc;
+
+  res.status(200).json(rest);
     } catch (error) {
       next (error)
     }
