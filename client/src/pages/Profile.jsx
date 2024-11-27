a948844d5fb9e79b76519589ffbffb5d5b45f2f7
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-27 22:00:41 +0300

    Implemented signout event handler

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -113,6 +113,18 @@ export default function Profile() {
     }
   }
 
+  const handleSignOut = async () => {
+    try {
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        return;
+      }
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -176,7 +188,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">{updateSuccess ? "Profile updated successfully" : ""}</p>
