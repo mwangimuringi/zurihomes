49ae5bd2cd7ea2194f7957db309841a3b64c3031
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-27 23:21:36 +0300

    Authenticate iser if is verified or not

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createListing } from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.post('/create', createListing);
+router.post('/create', verifyToken, createListing);
 
 export default router;
