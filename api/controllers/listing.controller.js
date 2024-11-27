89479fb20179aa8a6b077e8e8e2df9b633ebd769
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-27 23:42:44 +0300

    Created listings model

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,8 +1,9 @@
+import Listing from "../models/listing.model.js";
+
 export const createListing = async(req, res, next) => {
   
     try {
-        
-        
+        const listing = await Listing.create(req.body);
     } catch (error) {
         next(error);
     }
