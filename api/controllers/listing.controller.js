ff2af0412f6b20eda85e30ee385d719164582f93
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-27 23:47:00 +0300

    Tested the api route on insomnia

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -4,6 +4,7 @@ export const createListing = async(req, res, next) => {
   
     try {
         const listing = await Listing.create(req.body);
+        return res.status(201).json(listing);
     } catch (error) {
         next(error);
     }
