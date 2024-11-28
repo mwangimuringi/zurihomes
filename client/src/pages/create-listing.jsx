f43e3895c06fefbcd739c0bb1982a4bf02660801
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-28 10:34:03 +0300

    Created and styled listing form inputs

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
 
 export default function CreateListing() {
   return (
-    <div>
-      Create Listing
-    </div>
-  )
+    <main className="p-3 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form className="flex flex-col sm:flex-row ">
+        <div className="flex flex-col gap-4 flex-1">
+          <input
+            type="text"
+            placeholder="Name"
+            className="border p-3 rounded-lg"
+            id="name"
+            maxLength="62"
+            minLength="10"
+            required
+          />
+          <textarea
+            type="text"
+            placeholder="Description"
+            className="border p-3 rounded-lg"
+            id="description"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            className="border p-3 rounded-lg"
+            id="address"
+            required
+          />
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input type="checkbox" className="w-5" id="sale" />
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" className="w-5" id="rent" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" className="w-5" id="parking" />
+              <span>Parking spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" className="w-5" id="furnished" />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" className="w-5" id="offer" />
+              <span>Offer</span>
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
 }
