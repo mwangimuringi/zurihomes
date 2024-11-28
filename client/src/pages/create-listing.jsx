0988079c6872f02a3c70af8114ea1125bcfe4195
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-28 11:55:18 +0300

    Add Image Upload and Submit Button to CreateListing Component

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -6,7 +6,7 @@ export default function CreateListing() {
       <h1 className="text-3xl font-semibold text-center my-7">
         Create a Listing
       </h1>
-      <form className="flex flex-col sm:flex-row ">
+      <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
@@ -105,6 +105,29 @@ export default function CreateListing() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold text-center">
+            Upload images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max 6)
+            </span>
+          </p>
+          <div className="flex gap-4 flex-wrap">
+            <input
+              className="p-3 border border-gray-300 rounded w-full"
+              type="file"
+              id="images"
+              accept="images/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create Listing
+          </button>
         </div>
       </form>
     </main>
