fddd5dc312591c8f5dc3b4dc32d0a9a1a96ed552
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-29 15:47:52 +0300

    Implemented upload method to firebase storage

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
@@ -10,6 +12,38 @@ export default function CreateListing() {
     }
   }
   };
+  const storeimage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          console.log("Upload is complete");
+          resolve(fileName);
+        }
+      )
+    })
+  }
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
