59041593425a2bd3f79b930e424c4cfc0e249afd
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-29 15:14:01 +0300

    Stored files on firebase

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
-  console.log(files);
+  const handleImageSubmit = (e) => {
+    
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -117,14 +119,18 @@ export default function CreateListing() {
           </p>
           <div className="flex gap-4 flex-wrap">
             <input
-            onChange={(e) => setFiles(e.target.files)}
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="images/*"
               multiple
             />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+            <button
+              type="button"
+              onClick={handleImageSubmit}
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
               Upload
             </button>
           </div>
