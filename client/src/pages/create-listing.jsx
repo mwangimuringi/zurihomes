588dd72ebce951383b4e74de29c703a3567e1790
mwangimuringi <mwangimuringi10@gmail.com>
2024-11-29 15:52:18 +0300

    Removed the progress in file upload

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -20,26 +20,6 @@ export default function CreateListing() {
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
         "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          console.log("Upload is complete");
-          resolve(fileName);
-        }
       )
     })
   }
