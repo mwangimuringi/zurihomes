11aa12da3d9bd7438dbb7c6c26090308deb29f03
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-04 21:14:30 +0300

    Implemented error handler

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async(req, res, next) => {
   
@@ -13,6 +14,11 @@ export const deleteListing = async(req, res, next) => {
     
         const listing = await Listing.findById(req.params.id);
         if (!listing) {
-            return res.status(404).json({ message: "Listing not found" });
+            return next(errorHandler(401, 'You are not authorized to delete this listing'));
+        }
+        try {
+            await listing.findByIdAndDelete(req.params.id);
+        } catch (error) {
+            next(error);
         }
 };
