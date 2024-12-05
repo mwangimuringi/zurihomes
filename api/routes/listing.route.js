ab64afd4458adefa2e1aab667280a7e9ef27a90f
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-05 23:45:00 +0300

    Fixed delete listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
-router.delete('/delete/:id', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
+// router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
