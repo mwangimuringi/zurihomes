5094f7fa61036db6084ea0766ab5028ea8c6eb97
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-07 12:07:36 +0300

    Fetch listing data by ID in useEffect with error handling

--- client/src/pages/Update-listing.jsx
+++ client/src/pages/Update-listing.jsx
@@ -36,7 +36,12 @@ export default function UpdateListing() {
   useEffect(() => {
     const fetchListing = async () => {
       const listingId = params.listingId;
-      console.log(listingId);
+      const res = await fetch(`/api/listing/get/${listingId}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+      }
+      setFormData(data);
     };
     fetchListing();
   }, [id]);
