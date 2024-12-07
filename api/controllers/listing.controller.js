aba68359bd1aad43625c48d036464b375e9abc14
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-07 11:56:20 +0300

    Added update-listing api route.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -47,3 +47,17 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(
+        errorHandler(404, "Listing not found, cannot update this listing")
+      );
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
