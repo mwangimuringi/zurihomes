6dbe2a3c3ca1efb6d0fe394ef16a821288d30836
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-10 23:41:31 +0300

    Integrated useEffect on fetching of data

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Listing() {
+    useEffect(() => {
+        const fetchListings = async () => {
+            const response = await fetch('/api/listings')
+            const data = await response.json()
+            console.log(data)
+        }
+        fetchListings()
+    }, [])
   return (
     <div>
       
