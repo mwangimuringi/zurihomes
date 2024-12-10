f8f4cd350147106579a1661c9194731f9d2a8bf4
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-10 23:47:32 +0300

    ...

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Listing() {
+    const params = useParams()
     useEffect(() => {
         const fetchListings = async () => {
-            const response = await fetch(`/api/listings/get/${}`)
+            const response = await fetch(`/api/listings/get/${params.listingId}`)
         }
         fetchListings()
     }, [])
