76eb4bb3f4a4186149255fc8f826a106591dc3a3
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-12 23:14:24 +0300

    Implemented  loading effect on Listing

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Listing() {
-    const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
-    const params = useParams()
-    useEffect(() => {
-        const fetchListings = async () => {
-            try {
-                setLoading(true)
-                const response = await fetch(`/api/listings/get/${params.listingId}`)
-                const data = await response.json()
-                if (data.success === false) {
-                    setError(true)
-                    setLoading(false)
-                    return;
-                }
-                setListing(data)
-            } catch (error) {
-                setError(true);
-            }
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`/api/listings/get/${params.listingId}`);
+        const data = await response.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
         }
-        fetchListings()
-    }, [])
+        setListing(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListings();
+  }, [params.listingId]);
+  console.log(loading);
+
   return (
     <main>
-        {listing && listing.name}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
     </main>
-  )
+  );
 }
