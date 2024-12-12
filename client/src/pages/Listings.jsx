96b44903cf178ae3ce86427f80c67b71326208d9
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-12 23:54:17 +0300

    Imported useful swiper components

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import {Navigation} from "swiper/modules";
+import "swiper/css/bundle";
 
 export default function Listing() {
   const [listing, setListing] = useState(null);
@@ -32,10 +36,10 @@ export default function Listing() {
   return (
     <main>
       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-      {error && <p className="text-center my-7 text-2xl">Something went wrong!</p>}
-      {listing && !loading && !error && (
-        <h1>{listing.name}</h1>
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
       )}
+      {listing && !loading && !error && <h1>{listing.name}</h1>}
     </main>
   );
 }
