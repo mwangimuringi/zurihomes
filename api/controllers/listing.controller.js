8e6dbeb43facf9594b8ef7a2c6662ee06658260c
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-17 22:03:47 +0300

    Implement filters for offer, furnished, parking, and type in getListings API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,6 +64,29 @@ export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
 
     const listings = await Listing.find({
       name: { $regex: searchTerm, $options: "i" },
@@ -72,7 +95,7 @@ export const getListings = async (req, res, next) => {
       parking,
       type,
     })
-      .sort({ [sort]: order })
+      .sort({ [sort]: order })// Sorting (not part of filtering but essential).
       .limit(limit)// Pagination: Limits the number of documents retur
       .skip(startIndex);
 
