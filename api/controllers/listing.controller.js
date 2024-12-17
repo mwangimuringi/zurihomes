f5e8d0a2f9ea880503a7f6df53254abf9c73c77f
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-17 21:52:54 +0300

    Fixed semantics error

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -38,9 +38,9 @@ export const updateListing = async (req, res, next) => {
   }
   try {
     const updatedListing = await Listing.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
+      req.params.id,
+      req.body,
+      { new: true }
     );
     res.status(200).json(updatedListing);
   } catch (error) {
@@ -52,21 +52,7 @@ export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(
-        errorHandler(404, "Listing not found, cannot update this listing")
-      );
-    }
-    res.status(200).json(listing);
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const getListings = async (req, res, next) => {
-  try {
-    const listing = await Listing.findById(req.params.id);
-    if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
     res.status(200).json(listing);
   } catch (error) {
