379734f261ba3528f06b8d9c3f726a1b2a53e516
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-18 12:24:11 +0300

    Enhance the results section for listings.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -49,20 +49,20 @@ export default function Search() {
           <div className="flex items-center gap-2">
             <label className="font-semibold">Sort By:</label>
             <select id="sort_order" className="border rounded-lg p-3">
-                <option>Price high to low</option>
-                <option>Price low to high</option>
-                <option>Latest</option>
-                <option>Oldest</option>
+              <option>Price high to low</option>
+              <option>Price low to high</option>
+              <option>Latest</option>
+              <option>Oldest</option>
             </select>
           </div>
-          <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
             Search
           </button>
         </form>
       </div>
       <div className="">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
-          Search Results:
+          Listing results:
         </h1>
       </div>
     </div>
