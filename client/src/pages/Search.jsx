b49bc051f097dd5c0c7ce107896ea08baa4e3996
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-18 17:02:18 +0300

    Initial state and URL handling

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 
 export default function Search() {
+  const [sidebardata, setSidebarData] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    setSidebarData({
+      searchTerm: urlParams.get("searchTerm") || "",
+      type: urlParams.get("type") || "all",
+      parking: urlParams.get("parking") === "true",
+      furnished: urlParams.get("furnished") === "true",
+      offer: urlParams.get("offer") === "true",
+      sort: urlParams.get("sort") || "created_at",
+      order: urlParams.get("order") || "desc",
+    });
+  }, [location.search]);
+  
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
