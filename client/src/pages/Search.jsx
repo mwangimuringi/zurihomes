32857b20678eeb4b3ebf340fba08be0afb8258d2
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-18 11:58:40 +0300

    Add search term input field to the form.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function Search() {
-    return (
-        <div className="flex flex-col md:flex-row">
-            <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
-                <form className="flex flex-col gap-8">
-                </form>
-            </div>
-            <div className="">
-                <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Search Results</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+        <form className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
+            <input
+              type="text"
+              id="searchTerm"
+              placeholder="Search..."
+              className="border rounded-lg p-3 w-full"
+            />
+          </div>
+        </form>
+      </div>
+      <div className="">
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
+          Search Results:
+        </h1>
+      </div>
+    </div>
+  );
 }
