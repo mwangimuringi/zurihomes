ae1378de559c49de73899d8b2ac9455285f27e33
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-20 21:42:39 +0300

    Add Bedroom and Bathroom Information.

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -34,6 +34,18 @@ export default function ListingItem({ listing }) {
                 : listing.regularPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
             </p>
+            <div className="text-slate-700 flex gap-4">
+              <div className="font-bold text-xs">
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} beds `
+                  : `${listing.bedrooms} bed `}
+              </div>
+              <div className="font-bold text-xs">
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} baths `
+                  : `${listing.bathrooms} bath `}
+              </div>
+            </div>
           </div>
         </Link>
       </div>
