0e976a991fe61789f522d16c6117124f7d0f3294
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-20 21:39:54 +0300

    Add Listing Image with Fallback Support

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default function ListingItem({ listing }) {
   return (
-    <div>
-      <Link to={`/listing/${listing._id}`}>View Listing</Link>
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={
+            listing.imageUrls[0] ||
+            'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
+          }
+          alt="listing cover"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
+        />
+      </Link>
     </div>
   );
 }
