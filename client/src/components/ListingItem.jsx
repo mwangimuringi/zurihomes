844526bec89bdf9fc948f97a9685c85a2cb0d938
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-20 21:39:09 +0300

    Integrate React Router Link for Navigation

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function ListingItem({ listing }) {
-  return <div></div>;
+  return (
+    <div>
+      <Link to={`/listing/${listing._id}`}>View Listing</Link>
+    </div>
+  );
 }
