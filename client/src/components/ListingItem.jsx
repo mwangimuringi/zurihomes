3ecc73d18c17cc0edd62050b705544a8ad8ec0ad
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-20 21:40:53 +0300

    Display Listing Details (Name, Address, and Description)

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MdLocationOn } from 'react-icons/md';
 
 export default function ListingItem({ listing }) {
   return (
@@ -13,6 +14,20 @@ export default function ListingItem({ listing }) {
           alt="listing cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
+        <div className="p-3 flex flex-col gap-2 w-full">
+          <p className="truncate text-lg font-semibold text-slate-700">
+            {listing.name}
+          </p>
+          <div className="flex items-center gap-1">
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="text-sm text-gray-600 truncate w-full">
+              {listing.address}
+            </p>
+          </div>
+          <p className="text-sm text-gray-600 line-clamp-2">
+            {listing.description}
+          </p>
+        </div>
       </Link>
     </div>
   );
