7c0a921412d17d79942eaed6afde929fa648c0ab
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-21 23:09:49 +0300

    Add hero section to Home page with tagline and CTA link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
 
 export default function Home() {
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        setLoading(true);
+        setShowMore(false); // Reset showMore when a new search is performed
+  
+        const urlParams = new URLSearchParams(location.search);
+        const res = await fetch(`/api/listing/get?${urlParams.toString()}`);
+  
+        if (!res.ok) {
+          throw new Error(`Failed to fetch listings: ${res.statusText}`);
+        }
+  
+        const data = await res.json();
+        if (data.length > 8) {
+          setShowMore(true); // Enable "Show More" if more than 8 results
+        }
+        setListings(data);
+      } catch (error) {
+        console.error("Error fetching listings:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchListings();
+  }, [location.search]);
   return (
     <div>
       Home
