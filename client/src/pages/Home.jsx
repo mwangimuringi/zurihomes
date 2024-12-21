1730347d2e5fb0195fcbd73a431bd4c7f327c9cd
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-21 23:16:26 +0300

    Integrate Swiper for offer listings carousel.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -77,5 +77,22 @@ export default function Home() {
         )}
       </div>
     </div>
+    {/* Swiper */}
+<Swiper navigation>
+  {offerListings &&
+    offerListings.length > 0 &&
+    offerListings.map((listing) => (
+      <SwiperSlide>
+        <div
+          style={{
+            background: `url(${listing.imageUrls[0]}) center no-repeat`,
+            backgroundSize: 'cover',
+          }}
+          className='h-[500px]'
+          key={listing._id}
+        ></div>
+      </SwiperSlide>
+    ))}
+</Swiper>
   );  
 }
