2bff4f9fd04a589d28e1c036f4a9833386fa6d45
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-22 11:56:08 +0300

    Add detailed content to the About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      About
+      <h1>About ZuriHomes</h1>
+      <p>ZuriHomes is a platform that connects people with homes for rent or sale...</p>
+      <p>Our mission is to help our clients achieve their real estate goals...</p>
+      <p>At ZuriHomes, we believe the best way to find the perfect home...</p>
     </div>
   );
 }
