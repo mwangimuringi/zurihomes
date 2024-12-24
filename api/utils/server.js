bd350051b7b1117a4a97987b026c7bdb929e7aba
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-24 13:36:16 +0300

    Initialized firebase admin

---
+++ api/utils/server.js
@@ -0,0 +1,16 @@
+import admin from 'firebase-admin';
+
+// Initialize Firebase Admin SDK
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+} else {
+  admin.app(); // If already initialized, use the default app
+}
+
+export default admin;
