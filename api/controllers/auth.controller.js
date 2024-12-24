0b111053309a22e26ba17be727d4b5297f143541
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-24 12:22:45 +0300

    Refactored signin function for better error handling and response formatting

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,13 +34,13 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid password"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: pswd, ...rest } = validUser._doc;
+    const { password, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json(rest);
+      .json({ message: "User signed in", user: rest });
   } catch (error) {
     next(error);
   }
