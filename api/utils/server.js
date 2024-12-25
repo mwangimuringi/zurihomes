a5e17453ad06c9d2b8e3ddfee3e66c37c98342d7
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-25 12:12:08 +0300

    Added Firebase Admin SDK setup

--- api/utils/server.js
+++ api/utils/server.js
@@ -1,16 +1,12 @@
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 
-// Initialize Firebase Admin SDK
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    }),
-  });
-} else {
-  admin.app(); // If already initialized, use the default app
-}
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
-export default admin;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
