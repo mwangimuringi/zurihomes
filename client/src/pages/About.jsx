cf057d6f2836c33b5baf0090d7a50165888cb8dd
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-28 23:56:21 +0300

    Added introduction paragraph.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,6 +4,11 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">About ZuriHomes</h1>
+      <p className="text-slate-700 mb-4">
+        ZuriHomes is a platform that connects people with homes for rent or
+        sale. It is a web application that allows users to browse listings and
+        make offers and purchases.
+      </p>
     </div>
   );
 }
