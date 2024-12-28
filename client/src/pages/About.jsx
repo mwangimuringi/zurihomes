ee73e4e3dd82188af4f0b6d712d8e7dc3c178bab
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-28 23:55:36 +0300

    Create the About.js Component.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,32 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About ZuriHomes
-      </h1>
-      <p className="text-slate-700 mb-4">
-        ZuriHomes is a platform that connects people with homes for rent or
-        sale. It is a web application that allows users to browse listings and
-        make offers and purchases. Our team is working on building a scalable,
-        secure, and reliable platform that will allow people to easily find and
-        rent out their homes.
-      </p>
-      <p className="text-slate-700 mb-4">
-        Our mission is to help our clients achieve their real estate goals by
-        providing expert advice, personalized service, and a deep understanding
-        of the local market. Whether you are looking to buy, sell, or rent a
-        property, we are here to help you every step of the way. Our team is
-        dedicated to providing exceptional customer service and support, and we
-        are committed to helping our clients achieve their goals.
-      </p>
-      <p className="text-slate-700 mb-4">
-        At ZuriHomes, we believe that the best way to find the perfect home for
-        you is to get to know the people who live there. We offer a wide range
-        of properties for sale, from apartments to houses, and we are committed
-        to helping our clients find the perfect place to live. Whether you are
-        looking for a new home or an upgrade, we are here to help you every step
-        of the way.
-      </p>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About ZuriHomes</h1>
     </div>
   );
-} 
+}
