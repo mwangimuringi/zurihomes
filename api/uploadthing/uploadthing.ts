02e501d98c7ca3e6b31970f690a20d03c346d2f3
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-30 23:35:43 +0300

    Using FileRouter in frontend

--- api/uploadthing/uploadthing.ts
+++ api/uploadthing/uploadthing.ts
@@ -3,14 +3,15 @@ import type { FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
 
-export const uploadRouter = {
-  imageUploader: f({
-    allowedFileTypes: ["image"], 
-    maxFileSize: "4MB",       
-    maxFileCount: 6,            
-  }).onUploadComplete(({ metadata, file }) => {
-    console.log("Upload completed:", { metadata, file });
-  }),
-} satisfies FileRouter;
+export const uploadRouter: FileRouter = {
+  imageUploader: f()
+    .onUploadStart(({ file }) => {
+      // Optionally validate the file here
+      console.log("Upload started:", file.name);
+    })
+    .onUploadComplete(({ file }) => {
+      console.log("Upload completed:", file);
+    }),
+};
 
 export type OurFileRouter = typeof uploadRouter;
