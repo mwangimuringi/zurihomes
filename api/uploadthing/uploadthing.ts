87c50b06227955872f5f3070bda83e0c152f4d71
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-30 23:17:08 +0300

    Configured uploadthing for backend.

--- api/uploadthing/uploadthing.ts
+++ api/uploadthing/uploadthing.ts
@@ -1,1 +1,12 @@
-//add uploadthing file
+import { createUploadthing } from "uploadthing/express";
+
+const uploadthing = createUploadthing({
+  image: {
+    maxFileSize: "4MB",
+    maxFileCount: 4,
+  },
+}).onUploadComplete((data) => {
+  console.log("Upload completed:", data);
+});
+
+export default uploadthing;
