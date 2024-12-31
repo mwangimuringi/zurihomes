c3cc448be72a9cf9a94bb2eb5272ca57092b93f8
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-31 14:05:12 +0300

    Implemented error handling on storeImage function

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -59,13 +59,26 @@ export default function CreateListing() {
 
   const storeImage = async (file) => {
     return new Promise((resolve, reject) => {
-      uploadRouter.imageUploader({
-        file,
-      }).onUploadComplete((data) => {
-        resolve(data.url); 
-      }).onError((error) => {
-        reject(error);
-      });
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
     });
   };
 
