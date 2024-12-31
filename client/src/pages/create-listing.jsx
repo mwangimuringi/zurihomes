43c19974d6d99c3947848c5fe60dae50547d0bc9
mwangimuringi <mwangimuringi10@gmail.com>
2024-12-31 13:37:57 +0300

    Integrated storeImage function properly

--- client/src/pages/create-listing.jsx
+++ client/src/pages/create-listing.jsx
@@ -45,7 +45,7 @@ export default function CreateListing() {
           setUploading(false);
         })
         .catch((err) => {
-          setImageUploadError('Image upload failed (2 mb max per image)');
+          setImageUploadError('Image upload failed (5 mb max per image)');
           setUploading(false);
         });
     } else {
@@ -56,26 +56,13 @@ export default function CreateListing() {
 
   const storeImage = async (file) => {
     return new Promise((resolve, reject) => {
-      const storage = getStorage(app);
-      const fileName = new Date().getTime() + file.name;
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL);
-          });
-        }
-      );
+      uploadRouter.imageUploader({
+        file,
+      }).onUploadComplete((data) => {
+        resolve(data.url); 
+      }).onError((error) => {
+        reject(error);
+      });
     });
   };
 
