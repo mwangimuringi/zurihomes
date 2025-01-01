81a4a9863f4b4911becfe4958a0a1e8b9f770384
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-01 22:58:26 +0300

    Replace Firebase with UploadThing for profile picture uploads.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,6 +141,24 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listings/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message); // Add more later
+    }
+  };
+  
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
