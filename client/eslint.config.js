164816a00d7416b4e00ec672726ba664af938d4d
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-03 09:58:12 +0300

    update configuration to ignore specific rules and directories

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules', 'src/legacy/**'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -33,6 +33,10 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // Disable prop-types validation
+      'no-unused-vars': 'warn', // Show as a warning, not an error
+      'react/no-unescaped-entities': 'warn', // Show as a warning
+      'no-undef': 'warn', // Show as a warning for undefined variables
     },
   },
-]
+];
